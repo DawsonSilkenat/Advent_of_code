36882935e89509abf0ab961fd79d7e38c6cad08f
Dawson <dawsonsilkenat@gmail.com>
2023-12-04 13:35:01 +0000

    Completed part 2

--- 2023/solutions/solution_1.py
+++ 2023/solutions/solution_1.py
@@ -1,12 +1,22 @@
 import os
 
-test_answer = 142
+test_answer = 281
 filename = os.getcwd() + "/2023/inputs/" + "input_1.txt"
+
+str_digits = {"one" : "1", "two" : "2", "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9"}
+
 
 def get_number(line):
     first = None
     last = None 
-    for char in line:
+    for i in range(len(line)):
+        char = line[i]
+        for k in str_digits.keys():
+            if line[i : i + len(k)] == k:
+                char = str_digits[k]
+                break
+        
+        
         if char.isdigit():
             if first is None:
                 first = char 
